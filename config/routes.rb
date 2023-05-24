ae780840e50a2b8e4468db12e259fc7d0d0d9f91
Patrick Pruessen <pruessen@gmx.de>
2023-05-24 17:18:34 +0200

    added reviews (still not showing)

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  #  resources :restaurants, except: %i[edit update destroy]
+  resources :restaurants, only: %i[show index new create] do
+    resources :reviews, only: %i[new create]
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+
+10.times do
+  restaurant = Restaurant.new
+  restaurant.name = Faker::TvShows::BrooklynNineNine.character
+  restaurant.address = Faker::Address.city
+  restaurant.phone_number = Faker::PhoneNumber.phone_number
+  restaurant.category = ["chinese", "italian", "japanese", "french", "belgian"].sample
+  restaurant.save!
+end
